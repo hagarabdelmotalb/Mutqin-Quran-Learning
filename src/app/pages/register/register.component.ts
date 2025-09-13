1ea011a55c3f03c4d62284f7f6aabdb49853acbb
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-09-14 00:55:23 +0300

    form validation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup,ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup,ReactiveFormsModule, Validators} from "@angular/forms";
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  register :FormGroup = new FormGroup({
-    name: new FormControl(null),
-    username: new FormControl(null),
-    email: new FormControl(null),
-    phone: new FormControl(null),
-    password: new FormControl(null),
-    password_confirmation: new FormControl(null),
+  registerFrom :FormGroup = new FormGroup({
+    name: new FormControl(null , [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
+    username: new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl(null , [Validators.required ,Validators.email]),
+    phone: new FormControl(null,[Validators.pattern(/^[0-9]{8,15}$/)]),
+    password: new FormControl(null, [Validators.required,Validators.minLength(6)]),
+    password_confirmation: new FormControl(null , [Validators.required]),
     role: new FormControl(null)
-  });
+  }, 
+);
 
-
-  //make the validation here
   submitForm() : void {
 
   }
