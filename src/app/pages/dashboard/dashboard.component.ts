bfee6aa287e3412ba2869a91e2170763e99d1f24
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-09-22 22:55:07 +0300

    dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
 }
+
